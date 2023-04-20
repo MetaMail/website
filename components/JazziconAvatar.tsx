6415da2c2b5ade817854a4e33896da0b5fb9bf9c
inlab <inlab@inlabdeiMac.local>
2023-04-20 09:23:09 +0800

    Syncavatar generation method to metamask

--- components/JazziconAvatar.tsx
+++ components/JazziconAvatar.tsx
@@ -4,10 +4,11 @@ import { getUserInfo } from '@utils/storage/user';
 
 const JazziconGrid:React.FC = () => {
     const containerRef = useRef<HTMLDivElement | null>(null);
-  
     useEffect(() => {
+        const address = getUserInfo().address?.slice(2,10)
+        const seed = parseInt(address??'', 16);
         const container = containerRef.current;
-        const el = jazzicon(24, Math.round(eval(getUserInfo()?.address??'').toString(16) * 10000000));
+        const el = jazzicon(24, seed);
         container?.appendChild(el);
 
         return () => {
