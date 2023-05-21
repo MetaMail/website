280efe7a4c771d5a05a982e73daa7e602cb20d16
bird <1069208936@qq.com>
2023-05-21 20:26:57 +0800

    移除测试代码

--- pages/context/index.tsx
+++ pages/context/index.tsx
@@ -2,7 +2,6 @@ import { createContext } from 'react';
 
 interface IMailBoxContext {
     removeAllState?: () => void;
-    name?: string;
 }
 const MailBoxContext = createContext<IMailBoxContext>({});
 export default MailBoxContext;
