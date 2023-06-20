1c6cd8aaacad205a0c8643eb0b2cf95acb332559
bird <1069208936@qq.com>
2023-06-20 22:49:07 +0800

    Modify layout framework

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,11 +1,15 @@
 import React, { ReactNode } from 'react';
 import Sidebar from './Sidebar';
+import Header from './Header';
 
 export default function Layout({ children }: { children: ReactNode }) {
     return (
-        <div className="w-screen h-screen flex flex-row">
+        <div className="font-poppins w-screen h-screen flex flex-row">
             <Sidebar />
-            <main>{children}</main>
+            <div className="flex flex-col flex-1">
+                <Header />
+                <main className="h-[calc(100vh-45px-28px)] bg-white w-[calc(100%-28px)] rounded-10">{children}</main>
+            </div>
         </div>
     );
 }
--- pages/mailbox/index.tsx
+++ pages/mailbox/index.tsx
@@ -1,9 +1,4 @@
-import React, { ReactElement, useEffect, useState, useContext } from 'react';
-import dynamic from 'next/dynamic';
-import { useRouter } from 'next/router';
-
 import { useMailListStore, useMailDetailStore, useNewMailStore } from 'lib/zustand-store';
-import { userSessionStorage, mailSessionStorage } from 'lib/session-storage';
 import { MailBoxContext } from 'context';
 
 import Layout from 'components/Layout';
@@ -12,67 +7,26 @@ import MailDetail from 'components/MailDetail';
 import NewMail from 'components/NewMail';
 
 export default function MailBoxPage() {
-    const JazziconGrid = dynamic(() => import('components/JazziconAvatar'), { ssr: false });
-    const router = useRouter();
-    const [address, setAddress] = useState<string>();
-    const { removeAllState } = useContext(MailBoxContext);
     const { isMailDetail } = useMailDetailStore();
     const { isWriting } = useNewMailStore();
+    const { setFilterType, resetPageIndex, setUnreadCount } = useMailListStore();
+    const { setDetailFromList, setDetailFromNew } = useMailDetailStore();
 
-    function getLogOut() {
-        userSessionStorage.clearUserInfo();
-        mailSessionStorage.clearMailListInfo();
-        removeAllState();
-        router.push('/');
-    }
-
-    useEffect(() => {
-        const address = userSessionStorage.getUserInfo()?.address;
-        if (!address) return getLogOut();
-        setAddress(address);
-    }, []);
-
-    return (
-        <div>
-            <div className="flex flex-col flex-1 h-screen pb-24 font-poppins pr-21 w-[calc(100vw-206px)] min-w-[700px]">
-                <div className="flex flex-row pt-10 justify-end">
-                    <div className="flex flex-row justify-end gap-4">
-                        <JazziconGrid size={24} addr={address} />
-                        <button
-                            className="flex text-md omit font-bold pb-6 mr-17 justify-between w-131"
-                            onClick={getLogOut}>
-                            <div className="omit pt-2">{address}</div>
-                        </button>
-                    </div>
-                </div>
-                <div className="flex flex-row flex-1 h-0 bg-white rounded-10">
-                    <MailList />
-                    {isMailDetail && <MailDetail />}
-                    {isWriting && <NewMail />}
-                </div>
-            </div>
-        </div>
-    );
-}
-
-MailBoxPage.getLayout = (page: ReactElement) => {
-    const RenderLayoutWithContext = () => {
-        const { setFilterType, resetPageIndex, setUnreadCount } = useMailListStore();
-        const { setDetailFromList, setDetailFromNew } = useMailDetailStore();
-
-        const removeAllState = () => {
-            setFilterType(0);
-            resetPageIndex();
-            setUnreadCount(0);
-            setDetailFromList(null);
-            setDetailFromNew(null);
-        };
-        return (
-            <MailBoxContext.Provider value={{ removeAllState }}>
-                <Layout>{page}</Layout>
-            </MailBoxContext.Provider>
-        );
+    const removeAllState = () => {
+        setFilterType(0);
+        resetPageIndex();
+        setUnreadCount(0);
+        setDetailFromList(null);
+        setDetailFromNew(null);
     };
 
-    return <RenderLayoutWithContext />;
-};
+    return (
+        <MailBoxContext.Provider value={{ removeAllState }}>
+            <Layout>
+                <MailList />
+                {isMailDetail && <MailDetail />}
+                {isWriting && <NewMail />}
+            </Layout>
+        </MailBoxContext.Provider>
+    );
+}
