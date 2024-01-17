f3ac06b28f0f9c1b51584ec71b3a5796cd43f8a1
weiweiying <weiying.wei@leadbank.com.cn>
2024-01-17 10:51:16 +0800

    bug fixed

--- components/Layout/Sidebar.tsx
+++ components/Layout/Sidebar.tsx
@@ -138,7 +138,6 @@ export default function Sidebar() {
       <div className="flex flex-col">
         <button className="flex items-center justify-center py-10">
           <Image src={logo} alt="logo" className="w-auto h-36 mr-3" />
-          {/* <Image src={logoBrand} alt="logo-brand" className="w-116" /> */}
           <p className="text-[27px] text-[#000] font-['PoppinsBold']  leading-[43px] h-36 dark:text-[#fff]">MetaMail</p>
         </button>
         <button className="btn-primary flex items-center justify-center text-white h-45 rounded-9 gap-9" onClick={handleClickNewMail}>
--- pages/welcome/index.tsx
+++ pages/welcome/index.tsx
@@ -96,7 +96,7 @@ export default function Welcome() {
         </div>
         <div className="pt-43 relative">
           <header className="flex flex-row justify-between px-40 lg:px-102">
-            <Image src={logoBrand} alt="logo" className="w-298 h-52" />
+            <Image src={logoBrand} alt="logo" width={298} height={52} />
             <div className="text-[16px] w-250 h-44 border border-[#1e1e1e] rounded-40 invisible lg:visible font-poppins flex items-center justify-center">
               <RainbowLogin content="Connect Wallet" />
             </div>
