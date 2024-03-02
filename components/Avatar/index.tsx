03f52a8426ddac4621f09ae795bd90463aa2c8c2
weiweiying <weiying.wei@leadbank.com.cn>
2024-03-02 21:54:06 +0800

    start头像区分

--- components/Avatar/index.tsx
+++ components/Avatar/index.tsx
@@ -3,7 +3,7 @@ import { useEffect, useRef } from 'react';
 const Avatar = ({ size, addr, className }: { size: number; addr?: string; className?: string }) => {
 
 
-  return <Avvvatars value={addr} />;
+  return <Avvvatars size={size} value={addr} displayValue={addr.charAt(0)} />;
 };
 
 export default Avatar;
