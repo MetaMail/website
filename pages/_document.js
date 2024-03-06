e3c7ffe78ce8e470068fa548d3071046abfb2c96
weiweiying <weiying.wei@leadbank.com.cn>
2024-03-06 20:45:03 +0800

    定义meta

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* 添加 meta 标签 */}
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
