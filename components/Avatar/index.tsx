528ef263207c924affb28b4d3abe3c9569f8a640
weiweiying <weiying.wei@leadbank.com.cn>
2024-03-06 17:36:54 +0800

    外来头像字号变大

--- components/Avatar/index.tsx
+++ components/Avatar/index.tsx
@@ -1,9 +1,10 @@
 import Avvvatars from 'avvvatars-react'
 import { useEffect, useRef } from 'react';
+import styles from './index.module.css'
 const Avatar = ({ size, addr, className }: { size: number; addr?: string; className?: string }) => {
 
 
-  return <Avvvatars size={size} value={addr} displayValue={addr.charAt(0)} />;
+  return <div className={styles.Avatar_customAvvvatars}><Avvvatars size={size} value={addr} displayValue={addr.charAt(0)} /></div>;
 };
 
 export default Avatar;
