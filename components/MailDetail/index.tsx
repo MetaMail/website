a181acec971c54c81a96d2bfc9335c847735379a
weiweiying <weiying.wei@leadbank.com.cn>
2024-03-18 16:05:37 +0800

    邮件详情不设置最大宽度

--- components/MailDetail/index.tsx
+++ components/MailDetail/index.tsx
@@ -415,11 +415,9 @@ export default function MailDetail() {
           <div className='relative flex justify-center text-left'>
             {<LoadingRing loading={loading} />}
             {
-              <div className='max-w-[800px] flex-1'>
-                <div className={`${loading ? `fadeOutAnima` : 'fadeInAnima'} flex-1 overflow-auto  text-lightMailContent dark:text-DarkMailContent`}>
-                  <div id="mailHtml" className='listContainer  box-border'>
-                    {selectedMail?.part_html ? parse(handleHighlineLink(DOMPurify.sanitize(selectedMail?.part_html, { ADD_ATTR: ['target'] }))) : selectedMail?.part_text}
-                  </div>
+              <div className={`${loading ? `fadeOutAnima` : 'fadeInAnima'} flex-1 overflow-auto  text-lightMailContent dark:text-DarkMailContent`}>
+                <div id="mailHtml" className='listContainer pl-[57px] pr-[5px] box-border'>
+                  {selectedMail?.part_html ? parse(handleHighlineLink(DOMPurify.sanitize(selectedMail?.part_html, { ADD_ATTR: ['target'] }))) : selectedMail?.part_text}
                 </div>
               </div>
             }
