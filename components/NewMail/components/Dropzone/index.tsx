1591406702ecf1abd770122c567c981e0e71d710
weiweiying <weiying.wei@leadbank.com.cn>
2024-04-12 17:01:41 +0800

    fix user reject sign,change account bug

--- components/NewMail/components/Dropzone/index.tsx
+++ components/NewMail/components/Dropzone/index.tsx
@@ -31,19 +31,9 @@ const UploadComponent = ({ onChange, isExtend }: IDropzone) => {
         <div {...getRootProps()}>
           <input {...getInputProps()} />
           {
-            // isExtend && (
-            //   <div className="dark:text-[#fff] flex-col text-[16px] text-[#1F2937] w-full h-[120px] border-2 rounded-[8px] border-dashed leading-24 border-dashed border-[#9CA3AF55] flex justify-center items-center">
-            //     <p className="font-poppinsSemiBold">Drop your files here or <span> browse </span></p>
-            //     <p className="text-[#9CA3AF] leading-[20px] font-['Poppins'] mt-5">Maximum size: 20MB</p>
-            //   </div>
-            // )
-          }
-          {
             (
               <label className="flex justify-center items-center btn-primary px-14 py-8 rounded-[8px] cursor-pointer bg-lightGrayBg dark:bg-[#ddd4]" title='Attach file'>
-                {/* <Image src={AddAttach} className="h-18" alt="上传文件" /> */}
                 <AddAttach height={20} width={20} fill={isDark ? '#f6f6f6' : '#222'} />
-                {/* <span className="ml-6 font-Poppins h-[18px] leading-[20px]">Attach</span> */}
               </label>
             )
           }
